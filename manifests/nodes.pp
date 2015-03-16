11f4839460e7d1520df26798502398730e307b95
Chris Barker <cbarker@puppetlabs.com>
2015-03-16 16:24:10 -0700

    removes extra $

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -3,7 +3,7 @@ class f5_profile::nodes {
   @@f5_node { "/Common/${ec2_instance_id}":
     ensure                   => 'present',
     address                  => $ipaddress,
-    description              => "Puppet Exported Server $${ec2_instance_id}",
+    description              => "Puppet Exported Server ${ec2_instance_id}",
     availability_requirement => 'all',
     health_monitors          => ['/Common/icmp'],
   }
